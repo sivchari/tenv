01305b7068808aec8ba77c22d81e5d0f09260388
sivchari <shibuuuu5@gmail.com>
2021-09-06 13:57:02 +0900

    impl linter

---
+++ plugin/main.go
@@ -0,0 +1,36 @@
+// This file can build as a plugin for golangci-lint by below command.
+//    go build -buildmode=plugin -o path_to_plugin_dir tenv/plugin/tenv
+// See: https://golangci-lint.run/contributing/new-linters/#how-to-add-a-private-linter-to-golangci-lint
+
+package main
+
+import (
+	"strings"
+
+	"golang.org/x/tools/go/analysis"
+
+	"github.com/sivchari/tenv"
+)
+
+// flags for Analyzer.Flag.
+// If you would like to specify flags for your plugin, you can put them via 'ldflags' as below.
+//     $ go build -buildmode=plugin -ldflags "-X 'main.flags=-opt val'" tenv/plugin/tenv
+var flags string
+
+// AnalyzerPlugin provides analyzers as a plugin.
+// It follows golangci-lint style plugin.
+var AnalyzerPlugin analyzerPlugin
+
+type analyzerPlugin struct{}
+
+func (analyzerPlugin) GetAnalyzers() []*analysis.Analyzer {
+	if flags != "" {
+		flagset := tenv.Analyzer.Flags
+		if err := flagset.Parse(strings.Split(flags, " ")); err != nil {
+			panic("cannot parse flags of tenv: " + err.Error())
+		}
+	}
+	return []*analysis.Analyzer{
+		tenv.Analyzer,
+	}
+}
---
+++ tenv_test.go
@@ -0,0 +1,16 @@
+package tenv_test
+
+import (
+	"testing"
+
+	"github.com/gostaticanalysis/testutil"
+	"golang.org/x/tools/go/analysis/analysistest"
+
+	"github.com/sivchari/tenv"
+)
+
+// TestAnalyzer is a test for Analyzer.
+func TestAnalyzer(t *testing.T) {
+	testdata := testutil.WithModules(t, analysistest.TestData(), nil)
+	analysistest.Run(t, testdata, tenv.Analyzer, "a")
+}
