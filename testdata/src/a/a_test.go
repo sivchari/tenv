aa86e4272c36c16b48a6662b8af61440b4148a2a
sivchari <shibuuuu5@gmail.com>
2021-09-19 04:13:43 +0900

    fix

--- testdata/src/a/a_test.go
+++ testdata/src/a/a_test.go
@@ -10,8 +10,8 @@ var (
 )
 
 func setup() {
-	os.Setenv("a", "b")        // if -all = true, want "func setup is not using testing.Setenv"
-	err := os.Setenv("a", "b") // if -all = true, want "func setup is not using testing.Setenv"
+	os.Setenv("a", "b")        // if -all = true, want  "os\\.Setenv\\(\\) can be replaced by `testing\\.Setenv\\(\\)` in setup"
+	err := os.Setenv("a", "b") // if -all = true, want  "os\\.Setenv\\(\\) can be replaced by `testing\\.Setenv\\(\\)` in setup"
 	if err != nil {
 		_ = err
 	}
@@ -20,7 +20,9 @@ func setup() {
 
 func TestF(t *testing.T) {
 	setup()
-	os.Setenv("a", "b")                         // want "os\\.Setenv\\(\\) can be replaced by `t\\.Setenv\\(\\)` in TestF"
+	os.Setenv("a", "b")        // want "os\\.Setenv\\(\\) can be replaced by `t\\.Setenv\\(\\)` in TestF"
+	err := os.Setenv("a", "b") // want "os\\.Setenv\\(\\) can be replaced by `t\\.Setenv\\(\\)` in TestF"
+	_ = err
 	if err := os.Setenv("a", "b"); err != nil { // want "os\\.Setenv\\(\\) can be replaced by `t\\.Setenv\\(\\)` in TestF"
 		_ = err
 	}
@@ -28,7 +30,9 @@ func TestF(t *testing.T) {
 
 func BenchmarkF(b *testing.B) {
 	testTB(b)
-	os.Setenv("a", "b")                         // want "os\\.Setenv\\(\\) can be replaced by `b\\.Setenv\\(\\)` in BenchmarkF"
+	os.Setenv("a", "b")        // want "os\\.Setenv\\(\\) can be replaced by `b\\.Setenv\\(\\)` in BenchmarkF"
+	err := os.Setenv("a", "b") // want "os\\.Setenv\\(\\) can be replaced by `b\\.Setenv\\(\\)` in BenchmarkF"
+	_ = err
 	if err := os.Setenv("a", "b"); err != nil { // want "os\\.Setenv\\(\\) can be replaced by `b\\.Setenv\\(\\)` in BenchmarkF"
 		_ = err
 	}
