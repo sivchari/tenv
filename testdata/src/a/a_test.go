6c90238957b33e590203a36d1e2aa29327e6310e
sivchari <shibuuuu5@gmail.com>
2021-09-19 03:17:49 +0900

    wip

--- testdata/src/a/a_test.go
+++ testdata/src/a/a_test.go
@@ -6,9 +6,7 @@ import (
 )
 
 var (
-	e   = os.Setenv("a", "b") // if -all = true, want "variable e is not using testing.Setenv"
-	_   = e
-	env string
+	e = os.Setenv("a", "b") // never seen
 )
 
 func setup() {
@@ -17,26 +15,25 @@ func setup() {
 	if err != nil {
 		_ = err
 	}
-	env = os.Getenv("a")
 	os.Setenv("a", "b") // if -all = true, "func setup is not using testing.Setenv"
 }
 
 func TestF(t *testing.T) {
 	setup()
-	os.Setenv("a", "b")                         // want "func TestF is not using testing.Setenv"
-	if err := os.Setenv("a", "b"); err != nil { // want "func TestF is not using testing.Setenv"
+	os.Setenv("a", "b")                         // want "os.Setenv() can be replaced by `t.Setenv()` in TestF"
+	if err := os.Setenv("a", "b"); err != nil { // want "os.Setenv() can be replaced by `t.Setenv()` in TestF"
 		_ = err
 	}
 }
 
 func BenchmarkF(b *testing.B) {
 	testTB(b)
-	os.Setenv("a", "b")                         // want "func BenchmarkF is not using testing.Setenv"
-	if err := os.Setenv("a", "b"); err != nil { // want "func BenchmarkF is not using testing.Setenv"
+	os.Setenv("a", "b")                         // want "os.Setenv() can be replaced by `b.Setenv()` in BenchmarkF"
+	if err := os.Setenv("a", "b"); err != nil { // want "os.Setenv() can be replaced by `b.Setenv()` in BenchmarkF"
 		_ = err
 	}
 }
 
 func testTB(tb testing.TB) {
-	os.Setenv("a", "b") // want "func testTB is not using testing.Setenv"
+	os.Setenv("a", "b") // want "os.Setenv() can be replaced by `tb.Setenv()` in testTB"
 }
