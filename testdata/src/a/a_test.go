de70c8dd1162fa743d520287574d8b0ecc078a18
sivchari <shibuuuu5@gmail.com>
2022-04-02 13:43:33 +0900

    apply testing.F

--- testdata/src/a/a_test.go
+++ testdata/src/a/a_test.go
@@ -46,3 +46,12 @@ func TB(tb testing.TB) {
 		_ = err
 	}
 }
+
+func FuzzF(f *testing.F) {
+	os.Setenv("a", "b")        // want "os\\.Setenv\\(\\) can be replaced by `f\\.Setenv\\(\\)` in FuzzF"
+	err := os.Setenv("a", "b") // want "os\\.Setenv\\(\\) can be replaced by `f\\.Setenv\\(\\)` in FuzzF"
+	_ = err
+	if err := os.Setenv("a", "b"); err != nil { // want "os\\.Setenv\\(\\) can be replaced by `f\\.Setenv\\(\\)` in FuzzF"
+		_ = err
+	}
+}
