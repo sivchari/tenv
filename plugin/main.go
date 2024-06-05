42efb1f9094b6bd3613ee5d4b5ce5ef2dd79a993
sivchari <shibuuuu5@gmail.com>
2024-06-05 11:44:24 +0900

    fix: lint
    Signed-off-by: sivchari <shibuuuu5@gmail.com>

--- plugin/main.go
+++ plugin/main.go
@@ -14,7 +14,7 @@ import (
 
 // flags for Analyzer.Flag.
 // If you would like to specify flags for your plugin, you can put them via 'ldflags' as below.
-//     $ go build -buildmode=plugin -ldflags "-X 'main.flags=-opt val'" tenv/plugin/tenv
+// $ go build -buildmode=plugin -ldflags "-X 'main.flags=-opt val'" tenv/plugin/tenv
 var flags string
 
 // AnalyzerPlugin provides analyzers as a plugin.
